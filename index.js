9e1055d607a7502068ad58f07bd6ddf12a8e55a6
Martin Freisen <martin.freisen@comspace.de>
2019-09-23 17:49:47 +0200

    fix language based space inconsistencies

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const keys = require('./keys.json');
+const timespanTranslations = require('./timespan-translations.json');
 
-function trim(str){
-	return str.replace(/^[\s\t]+/,'').replace(/[\s\t]+$/,'');
+function trim(str) {
+	return str.replace(/^[\s\t]+/, '').replace(/[\s\t]+$/, '');
 }
 
-function convertValue(val){
+function convertValue(val) {
 	const valueType = typeof val;
 	if (valueType === 'number') {
 		return val;
@@ -13,36 +14,46 @@ function convertValue(val){
 		if (val.match(/^[0-9]+$/)) {
 			return parseInt(val, 10);
 		}
-		return val;
+		return val.replace('_', ' '); // return tempTimeSpan to old value
 	}
 	throw new Error('Unexptected value type');
+}
+
+function tempTimeSpan(str) {
+	const targets = Object.keys(timespanTranslations);
+	targets.forEach(function (target) {
+		timespanTranslations[target].forEach(function (translation) {
+			str = str.replace(translation, target);
+		});
+	});
+	return str;
 }
 
 /*
 Handle both tab (normal) and space (sent using i.e. telegram) separated values
 */
-module.exports = function stringToJSON (str) {
+module.exports = function stringToJSON(str) {
 	const obj = {};
 	const lines = str.split('\n');
 	if (lines.length !== 2) {
 		throw new Error('Expect the string to have two lines.');
 	}
 	let headerLine = trim(lines[0]);
-	const valueLine = trim(lines[1]);
-	const values = valueLine.split(/[\s\t]/);
+	const valueLine = tempTimeSpan(trim(lines[1]));
+	const values = valueLine.split(/[\s\t]/).map(convertValue);
 	let keyIdx = 0;
-	
+
 	keys.forEach((key) => {
 		const keyPosition = headerLine.indexOf(key);
 		if (keyPosition > 0) {
 			throw new Error(`Key "${key}" should be missing or at beginning. Maybe an unknown key was added?\n${headerLine}`);
 		}
 		if (keyPosition === 0) { // key is at start. best case. no values needed to skip
-			obj[key] = convertValue(values[keyIdx++]);
+			obj[key] = values[keyIdx++];
 		} else if (keyPosition === -1) { // key was not found. agent might be missing this stat
 			obj[key] = 0;
 		}
-		headerLine = trim(headerLine.replace(key,''));
+		headerLine = trim(headerLine.replace(key, ''));
 	});
 	return obj;
 };
