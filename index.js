b678896b9cb71074dcad2893dbecde24371970a9
Martin Freisen <martin.freisen@gmail.com>
2023-10-16 19:15:22 +0200

    Parse uploads using tabs differently

--- index.js
+++ index.js
@@ -42,21 +42,33 @@ module.exports = function stringToJSON(str) {
 	}
 	let headerLine = trim(lines[0]);
 	const valueLine = tempTimeSpan(trim(lines[1]));
-	const values = valueLine.split(/[\s\t]/).map(convertValue);
-	let keyIdx = 0;
+	const containsTab = str.indexOf("\t");
+	if (containsTab) {
+		const headerElements = headerLine.split("\t");
+		const valueElements = valueLine.split("\t");
+		if (headerElements.length !== valueElements.length) {
+			throw new Error(`Difference between header (${headerElements.length}) and value (${valueElements.length}) element counts`);
+		}
+		for (let i = 0; i < headerElements.length; i++) {
+			obj[headerElements[i]] = valueElements[i];
+		}
+	} else {
+		const values = valueLine.split(/[\s\t]/).map(convertValue);
+		let keyIdx = 0;
 
-	keys.forEach((key) => {
-		const keyPosition = headerLine.indexOf(key);
-		if (keyPosition > 0) {
-			throw new Error(`Key "${key}" at position ${keyPosition} should be missing or at beginning. Maybe an unknown key was added?\n${headerLine}`);
-		}
-		if (keyPosition === 0) { // key is at start. best case. no values needed to skip
-			obj[key] = values[keyIdx++];
-		} else if (keyPosition === -1) { // key was not found. agent might be missing this stat
-			obj[key] = 0;
-		}
-		headerLine = trim(headerLine.replace(key, ''));
-	});
+		keys.forEach((key) => {
+			const keyPosition = headerLine.indexOf(key);
+			if (keyPosition > 0) {
+				throw new Error(`Key "${key}" at position ${keyPosition} should be missing or at beginning. Maybe an unknown key was added?\n${headerLine}`);
+			}
+			if (keyPosition === 0) { // key is at start. best case. no values needed to skip
+				obj[key] = values[keyIdx++];
+			} else if (keyPosition === -1) { // key was not found. agent might be missing this stat
+				obj[key] = 0;
+			}
+			headerLine = trim(headerLine.replace(key, ''));
+		});
+	}
 	requiredKeys.forEach((key) => {
 		if (!obj.hasOwnProperty(key)) {
 			throw new Error('Missing the required value ' + key);
